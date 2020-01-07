5a7fe0db182c05c68e3e5fc2d6ce563a85fa7f9f
akshat <akshatmehrotra2004@gmail.com>
2020-01-07 11:00:09 +0530

    Updated the model to increase accuracy

--- JULIA/judge a book/Judgeabookbyitscover.jl
+++ JULIA/judge a book/Judgeabookbyitscover.jl
@@ -1,5 +1,3 @@
-
-
 using Flux
 using Flux: @epochs, onehotbatch, crossentropy, throttle
 using Images
@@ -16,6 +14,7 @@ test_csv = "F:\\book-dataset\\Task1\\book30-listing-test1.csv"
 train_dataset  = CSV.read(train_csv) # read the csv for labels
                                      # The CSVs have 2 colums: first of genre and second of name of the book
                                      # The name of the book isnt required for the program but is included for debugging purposes
+test_dataset  = CSV.read(test_csv)
 
 train_imglist = readdir(train_path) # This will be used to find the total number of images in training set
 test_imglist = readdir(test_path) # In ideal scenario this will also be used for finding the total number of images in the testing set
@@ -29,15 +28,15 @@ imsize = 56
 epochs = 10
 
 function create_batch(indexs; path, csv, dataset)
-    X = Array{Float32}(undef, imsize, imsize, 3, length(indexs))
+    X = Array{Float32}(undef, imsize*imsize*3, length(indexs))
     for (p,i) in enumerate(indexs)
         img = load(string(path,i,".png")) # The images are labeled like 1.png, 2.png, and so on.
         img = channelview(RGB.(imresize(img, imsize, imsize)))
-        img = Float32.(permutedims(img, (2, 3, 1))) # we need the image in (PIXEL,PIXEL,CHANNEL) for it to be eligible to be added to array X
-                                                    # so we convert it from (CHANNEL, PIXEL,PIXEL) to the req. dims
-        X[:, :, :, p] = img # add the img to X
+        img = reshape(Float32.(img),(imsize*imsize*3)) # we need the image in (PIXEL,PIXEL,CHANNEL) for it to be eligible to be added to array X
+                                                       # so we convert it from (CHANNEL, PIXEL,PIXEL) to the req. dims
+        X[:, p] = img # add the img to X
     end
-    Y = onehotbatch(dataset[indexs[1]:indexs[end], 1], 0:29)
+    Y = onehotbatch(dataset[indexs, 1], 0:29)
     return (X, Y)
 end
 
@@ -50,25 +49,32 @@ test_set = create_batch(
     dataset = test_dataset
 )
 @info "creating the model"
+# m = Chain(
+#     Conv((3, 3), 3 => 32, pad = (1, 1), relu),
+#     MaxPool((2, 2)),
+#
+#     Conv((3, 3), 32 => 64, pad = (1, 1), relu),
+#     MaxPool((2, 2)),
+#
+#     Conv((2, 2), 64 => 128, pad = (1, 1), relu),
+#     MaxPool((2, 2)),
+#
+#     Conv((2, 2), 128 => 256, pad = (1, 1), relu),
+#     MaxPool((2, 2)),
+#
+#     x -> reshape(x, :, size(x, 4)),
+#     Dense(4096, 1024, relu),
+#     Dropout(0.5),
+#     Dense(1024, 512),
+#     Dense(512, 30),
+#     softmax,
+# )
+
 m = Chain(
-    Conv((3, 3), 3 => 32, pad = (1, 1), relu),
-    MaxPool((2, 2)),
-
-    Conv((3, 3), 32 => 64, pad = (1, 1), relu),
-    MaxPool((2, 2)),
-
-    Conv((2, 2), 64 => 128, pad = (1, 1), relu),
-    MaxPool((2, 2)),
-
-    Conv((2, 2), 128 => 256, pad = (1, 1), relu),
-    MaxPool((2, 2)),
-
-    x -> reshape(x, :, size(x, 4)),
-    Dense(4096, 1024, relu),
-    Dropout(0.5),
-    Dense(1024, 512),
-    Dense(512, 30),
-    softmax,
+    Dense(56*56*3, 512,relu),
+    Dense(512,128,relu),
+    Dense(128,30),
+    softmax
 )
 loss(x, y) = crossentropy(m(x), y)
 accuracy(x, y) = mean(Flux.onecold(m(x)) .== Flux.onecold(y))
