8ee01ecfbacdc4d8c4ce298ff198fa0ba87164a3
akshat <akshatmehrotra2004@gmail.com>
2020-01-07 14:20:18 +0530

    Replaced conv with dense

--- JULIA/judge a book/Judgeabookbyitscover.jl
+++ JULIA/judge a book/Judgeabookbyitscover.jl
@@ -1,92 +1,92 @@
+# After a lot of debugging and asking around
+# The fix was found to be to remove every conv layer.
+# Previously, whilst using the conv model, the accuracy was stuck at exactly 0.8 and loss at exactly 3.33253
+# Changing the model to be that of a simple dense one seems to bring the accuracy up to what is expected.
+# In the research paper, authors were able to achive a general accuracy of 24% in the model which they trained from the ground up
+# In doing so they used the whole 30000 image dataset. This model uses 1/3 of that datset and around 500 testing images because of processing constraints
+# The accuracy achived after 20 epochs was right at ~12%. Considering that it uses a third of the datset, this is pretty decent.
+
 using Flux
-using Flux: @epochs, onehotbatch, crossentropy, throttle
+using Flux: onehotbatch, crossentropy, throttle
 using Images
 using FileIO
 using CSV
 using Statistics
 
-train_path = "G:\\mlimgs\\Book-Train-FULL\\"# contains 10000 images
-train_csv = "F:\\book-dataset\\Task1\\book30-listing-train1.csv"
+const train_path = "G:\\mlimgs\\Book-Train-FULL\\" # contains the training images
+const train_csv = "F:\\book-dataset\\Task1\\book30-listing-train1.csv" # This file contains the labels(genre) for the training images
 
-test_path = "G:\\mlimgs\\Book-Test-FULL\\" #Contains 1000 images but we'll use 500
-test_csv = "F:\\book-dataset\\Task1\\book30-listing-test1.csv"
+const test_path = "G:\\mlimgs\\Book-Test-FULL\\" #Contains the testing dataset
+const test_csv = "F:\\book-dataset\\Task1\\book30-listing-test1.csv" # This file contains the labels(genre) for the testing images
 
-train_dataset  = CSV.read(train_csv) # read the csv for labels
-                                     # The CSVs have 2 colums: first of genre and second of name of the book
-                                     # The name of the book isnt required for the program but is included for debugging purposes
-test_dataset  = CSV.read(test_csv)
+const train_dataset  = CSV.read(train_csv)  # read the csv for training labels
+                                            # The CSVs have 2 colums: first of genre and second of name of the book
+                                            # The name of the book isn't required for the functioning of the model but is included for debugging purposes
 
-train_imglist = readdir(train_path) # This will be used to find the total number of images in training set
-test_imglist = readdir(test_path) # In ideal scenario this will also be used for finding the total number of images in the testing set
-# but we are only going to use 500 of those
+const test_dataset  = CSV.read(test_csv) # read the csv for testing labels
 
-train_setsize = length(train_imglist) # finding the size of training set
-test_setsize = 500
+# find the total number of images in sets so we can correctly divide the dataset into batches
+const train_imglist = readdir(train_path)
+const test_imglist = readdir(test_path)
+const train_setsize = length(train_imglist)
+const test_setsize = length(test_imglist)
 
-batch_size = 400
-imsize = 56
-epochs = 10
+# Self-explainatory Hyper Parameters
+const batch_size = 400
+const imsize = 60
+const epochs = 20
+const learning_rate = 0.0001
 
 function create_batch(indexs; path, csv, dataset)
-    X = Array{Float32}(undef, imsize*imsize*3, length(indexs))
+    X = Array{Float32}(undef, imsize*imsize*3, length(indexs))  # everytime this function is called a new batch is created with the correct size
+                                                                # It should be able to hold multiple flattened images (flattened because we're using a dense network)
+                                                                # Thats why it is shaped like (size_of_image,no._of_images)
     for (p,i) in enumerate(indexs)
         img = load(string(path,i,".png")) # The images are labeled like 1.png, 2.png, and so on.
         img = channelview(RGB.(imresize(img, imsize, imsize)))
-        img = reshape(Float32.(img),(imsize*imsize*3)) # we need the image in (PIXEL,PIXEL,CHANNEL) for it to be eligible to be added to array X
-                                                       # so we convert it from (CHANNEL, PIXEL,PIXEL) to the req. dims
-        X[:, p] = img # add the img to X
+        img = reshape(Float32.(img),(imsize*imsize*3))  # The current image has 3 layers of 60 by 60 pixels all compiled into a 3D array
+                                                        # We need the image in a flat array so we reshape into a flat array for it to be eligible to be added to array X
+        X[:, p] = img # add the img to X.
+
     end
     Y = onehotbatch(dataset[indexs, 1], 0:29)
     return (X, Y)
 end
 
-indexs = Base.Iterators.partition(1:train_setsize, batch_size)
+const indexs = Base.Iterators.partition(1:train_setsize, batch_size)
 
-test_set = create_batch(
+const test_set = create_batch(
     1:test_setsize;
     path = test_path,
     csv = test_csv,
     dataset = test_dataset
 )
+
 @info "creating the model"
-# m = Chain(
-#     Conv((3, 3), 3 => 32, pad = (1, 1), relu),
-#     MaxPool((2, 2)),
-#
-#     Conv((3, 3), 32 => 64, pad = (1, 1), relu),
-#     MaxPool((2, 2)),
-#
-#     Conv((2, 2), 64 => 128, pad = (1, 1), relu),
-#     MaxPool((2, 2)),
-#
-#     Conv((2, 2), 128 => 256, pad = (1, 1), relu),
-#     MaxPool((2, 2)),
-#
-#     x -> reshape(x, :, size(x, 4)),
-#     Dense(4096, 1024, relu),
-#     Dropout(0.5),
-#     Dense(1024, 512),
-#     Dense(512, 30),
-#     softmax,
-# )
+# I've tried using a conv net described in the paper but that
+# yields an accuracy of 0.8 and has a lot of inconsistencies  with it
+# People over at #julia-bridged (after a LONG thread of conversation) told me
+# to just change out the conv with a dense model. Sure enough, it started to behave like its supposed to
 
 m = Chain(
-    Dense(56*56*3, 512,relu),
-    Dense(512,128,relu),
-    Dense(128,30),
-    softmax
+    Dense(imsize*imsize*3, 512, relu), # we're expecting an image array
+    Dense(512, 64),
+    Dense(64, 30),
+    softmax,
 )
+
 loss(x, y) = crossentropy(m(x), y)
 accuracy(x, y) = mean(Flux.onecold(m(x)) .== Flux.onecold(y))
+
+opt = ADAM(learning_rate)
 
 function cbfunc()
     ca = accuracy(test_set...)
     print("batch_acc: ",string(ca),"; ")
     cl = loss(test_set...)
     println("batch_loss: ",string(cl))
-end
 
-opt = ADAM(0.0001)
+end
 
 for e in 1:epochs
     @info "Epoch no.-> $e"
@@ -94,7 +94,7 @@ for e in 1:epochs
     for i in indexs
         println("Batch no. -> $b")
         train_batch = [create_batch(i; path = train_path, csv = train_csv, dataset = train_dataset)] # we load every batch before training
-        # This way we dont have to load the whole big dataset into one array
+                                                                                                     # This way we dont have to load the whole big dataset into one array
         Flux.train!(loss, params(m), train_batch , opt, cb = cbfunc)
         b+=1
     end
